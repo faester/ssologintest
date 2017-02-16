890cceb2bccf6488229e616931f55bd09d467d82
Morten Fæster <faester@gmail.com>
2017-02-16 22:37:53 +0100

    logout etc

--- SsoLoginTest/Controllers/AccountController.cs
+++ SsoLoginTest/Controllers/AccountController.cs
@@ -1,6 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Transactions;
 using System.Web.Mvc;
 using System.Web.Security;
+using DotNetOpenAuth.AspNet;
 using Microsoft.Web.WebPages.OAuth;
 using SsoLoginTest.Sso;
 using WebMatrix.WebData;
